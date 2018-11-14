2b5f7b8f5cf41b2453d2577fa76d4583a16b0a78
Maryam Zakani <zakani91@gmail.com>
2018-11-14 12:09:55 +0100

    Add place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    BAR = 'b'
+    RESTAURANT = 'r'
+    THEATRE = 't'
+    CATEGORY_CHOICES = (
+        (BAR, 'Bar'),
+        (RESTAURANT, 'Restaurant'),
+        (THEATRE, 'Theatre'),
+    )
+
+    name = models.TextField()
+    description = models.TextField()
+    address = models.TextField()
+    coordinate_x = models.IntegerField()
+    coordinate_y = models.IntegerField()
+    map_id = models.IntegerField()
+    type = models.CharField(max_length=1, choices=CATEGORY_CHOICES)
+    # specific_type =
+    # neighborhood =
+    # cost
+    # working hour
+    photo = models.URLField()  # TODO: how many
+    poster = models.URLField()  # TODO: how many
