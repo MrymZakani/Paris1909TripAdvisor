8c72c8546cbd79aadb9e8c790c91e62087238415
Maryam Zakani <zakani91@gmail.com>
2018-11-21 11:50:23 +0100

    Add models to admin

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+from places.models import Neighborhood, MetroStation, Place, PlaceImage, Menu, MenuOption
+
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
+@admin.register(Neighborhood)
+class NeighborhoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+@admin.register(MetroStation)
+class MetroStationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'description', 'address',
+        'coordinate_x', 'coordinate_y', 'map_id', 'type',)
+    inlines = [PlaceImageInline, ]
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ('place', 'link', 'type')
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'place')
+
+
+@admin.register(MenuOption)
+class MenuOptionItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost', 'menu')
